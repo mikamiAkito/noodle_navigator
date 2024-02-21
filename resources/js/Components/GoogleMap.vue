6e1e8c75c646ea27e962b9973e6b11520d80b88d
Akito mikami <akitomikami@outlook.jp>
2024-02-21 19:19:00 +0900

    レスポンシブ対応

--- resources/js/Components/GoogleMap.vue
+++ resources/js/Components/GoogleMap.vue
@@ -1,155 +1,97 @@
 <script setup>
-import Modal from './Modal.vue';
-import { ref } from 'vue';
+let map;
+let infoWindow;
 
-const loadGoogleMapsAPI = () => {
-  // マップの初期設定
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
+//現在地取得処理
+const getCurrentlocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+async function initMap() {
+  //初期処理
+  const position = { lat: -25.344, lng: 131.031 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+  // const { SearchBox } = await google.maps.importLibrary("places");
+  //マップ読み込み
+  map = new Map(document.getElementById("map"), {
     zoom: 15,
-    mapTypeId: "roadmap",
+    center: position,
+    mapId: "DEMO_MAP_ID",
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
   });
-
-  //現在地取得準備
-  let infoWindow = new google.maps.InfoWindow();
-
-  // マーカーを管理する配列
-  let markers = [];
-
-  //現在地取得
-  const getCurrentlocation = () => {
-    markers.forEach(marker => marker.setMap(null));
-    markers = [];
-
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    }else{
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  }
-
-  // 検索ボックスをマップに追加
-  const input = document.getElementById("pac-input");
-  input.style.margin = "10px 10px 10px 10px";
-  const searchBox = new google.maps.places.SearchBox(input);
-  //現在地ボタンをマップへ追加
-  const current = document.getElementById("current-btn");
-  current.style.margin = "10px 10px 10px 10px";
-  //駅検索ボタンをマップへ追加
-  const stationbtn = document.getElementById("search-btn");
-  stationbtn.style.margin = "10px 10px 10px 10px";
-
-  // stationbtn.addEventListener("click", () => {
-
-  // });
-
-  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(input);
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(stationbtn);
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(current);
-
-  //ロード時現在地へ移動
+  infoWindow = new google.maps.InfoWindow();
+  //ボタン追加
+  const locationButton = document.getElementById("current-btn");
+  const searchButton = document.getElementById("search-btn");
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationButton);
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(searchButton);
+  //ロード時現在地へ
   getCurrentlocation();
-
-  //ボタンクリック時現在地へ移動
-  current.addEventListener("click", () => {
+  //クリック時現在地へ
+  locationButton.addEventListener("click", () => {
     getCurrentlocation();
   });
 
-  // マップの範囲が変更されたときに検索ボックスの範囲を更新
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  // 検索ボックスの検索結果が変更されたときにマーカーを更新
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length === 0) {
-      return;
-    }
-
-    // 古いマーカーをクリア
-    markers.forEach(marker => marker.setMap(null));
-    markers = [];
-
-    // 新しい検索結果に基づいてマーカーを追加
-    const bounds = new google.maps.LatLngBounds();
-    places.forEach(place => {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      markers.push(new google.maps.Marker({
-        map,
-        title: place.name,
-        position: place.geometry.location,
-      }));
-
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-
-    // マップを検索結果の範囲にフィット
-    map.fitBounds(bounds);
+  //マーカー設置
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
   });
 }
 
-// Google Maps API の読み込みを非同期で行う
-const script = document.createElement('script');
-script.src = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-script.onload = loadGoogleMapsAPI;
-document.head.appendChild(script);
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
 
-//モーダル処理
-const modalshow = ref(false);
+window.initMap = initMap;
 
-const openModal = () => {
-  modalshow.value = true;
-};
-
-const closeModal = () => {
-  modalshow.value = false;
-};
+initMap();
 </script>
 
 <template>
-  <div class="border-solid border-2 border-blue-700 flex justify-center">
+  <div>
     <div id="map" class=""></div>
-    <input id="pac-input" type="text" placeholder="Enter a location">
-    <div id="current-btn" class="border-solid border-2 border-blue-600 rounded-sm">
-      <button class="bg-blue-300 transition duration-700 hover:bg-blue-400 w-20 h-12 not-italic font-thin text-base">現在地</button>
+    <div class="">
+      <button id="search-btn" class="bg-white transition duration-700 hover:bg-blue-400 shadow-md w-10 h-10 rounded-sm m-2.5">
+        <svg viewBox="0 0 51 53" xmlns="http://www.w3.org/2000/svg" class="fill-blue-500 w-full h-2/3">
+          <path d="M49.3595 13.3609H46.9523V6.8985C46.9523 5.18457 46.1952 3.54083 44.8476 2.32889C43.5 1.11695 41.6723 0.436096 39.7665 0.436096L11.0233 0.436096C9.11753 0.436096 7.2898 1.11695 5.9422 2.32889C4.5946 3.54083 3.83753 5.18457 3.83753 6.8985V13.3609H1.39436C1.08943 13.3609 0.796992 13.4698 0.581376 13.6638C0.36576 13.8577 0.244629 14.1207 0.244629 14.3949V22.0205C0.244629 22.5925 0.758413 23.0545 1.39436 23.0545H3.83753V42.4417C3.83753 44.2189 3.83753 45.6729 7.43042 45.6729V50.5197C7.43042 50.9482 7.61969 51.3592 7.95659 51.6621C8.29349 51.9651 8.75043 52.1353 9.22687 52.1353H16.4127C16.8891 52.1353 17.3461 51.9651 17.683 51.6621C18.0198 51.3592 18.2091 50.9482 18.2091 50.5197V45.6729H32.5807V50.5197C32.5807 50.9482 32.77 51.3592 33.1069 51.6621C33.4438 51.9651 33.9007 52.1353 34.3772 52.1353H41.563C42.0394 52.1353 42.4963 51.9651 42.8332 51.6621C43.1701 51.3592 43.3594 50.9482 43.3594 50.5197V45.6729C46.9523 45.6729 46.9523 44.2189 46.9523 42.4417V23.0545H49.3595C49.674 23.0545 49.9756 22.9422 50.1979 22.7422C50.4203 22.5422 50.5452 22.271 50.5452 21.9882C50.5452 21.7054 50.4203 21.4342 50.1979 21.2342C49.9756 21.0343 49.674 20.9219 49.3595 20.9219C49.0451 20.9219 48.7435 21.0343 48.5212 21.2342C48.2988 21.4342 48.1739 21.7054 48.1739 21.9882C48.1739 22.271 48.2988 22.5422 48.5212 22.7422C48.7435 22.9422 49.0451 23.0545 49.3595 23.0545C49.674 23.0545 49.9756 22.9422 50.1979 22.7422C50.4203 22.5422 50.5452 22.271 50.5452 21.9882V14.4272C50.5452 14.1444 50.4203 13.8732 50.1979 13.6732C49.9756 13.4732 49.674 13.3609 49.3595 13.3609ZM18.2091 3.6673H32.5807V6.8985H18.2091V3.6673ZM11.0233 39.2105C10.0704 39.2105 9.15656 38.8701 8.48276 38.2641C7.80896 37.6582 7.43042 36.8363 7.43042 35.9793C7.43042 35.1224 7.80896 34.3005 8.48276 33.6945C9.15656 33.0886 10.0704 32.7481 11.0233 32.7481C11.9762 32.7481 12.8901 33.0886 13.5639 33.6945C14.2377 34.3005 14.6162 35.1224 14.6162 35.9793C14.6162 36.8363 14.2377 37.6582 13.5639 38.2641C12.8901 38.8701 11.9762 39.2105 11.0233 39.2105ZM7.43042 26.2857V10.1297H43.3594V26.2857H7.43042ZM39.7665 39.2105C38.8136 39.2105 37.8997 38.8701 37.2259 38.2641C36.5521 37.6582 36.1736 36.8363 36.1736 35.9793C36.1736 35.1224 36.5521 34.3005 37.2259 33.6945C37.8997 33.0886 38.8136 32.7481 39.7665 32.7481C40.7194 32.7481 41.6333 33.0886 42.3071 33.6945C42.9809 34.3005 43.3594 35.1224 43.3594 35.9793C43.3594 36.8363 42.9809 37.6582 42.3071 38.2641C41.6333 38.8701 40.7194 39.2105 39.7665 39.2105Z"/>
+        </svg>
+      </button>
     </div>
-    <div id="search-btn" class="border-solid border-2 border-red-600 rounded-sm">
-      <button @click="openModal" class="bg-red-300 transition duration-700 hover:bg-red-400 w-20 h-12 not-italic font-thin text-base">駅検索</button>
-    </div>
-    <!--モーダル-->    
-    <Modal :show="modalshow" @close="closeModal">
-      <p>This is the modal content.</p>
-      <button @click="closeModal" class="bg-red-700">閉じる</button>
-    </Modal>
-
+    <button id="current-btn" class="bg-white transition duration-700 hover:bg-red-400 shadow-md w-10 h-10 rounded-sm m-2.5">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-red-500 w-full h-2/3">
+        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+      </svg>
+    </button>
   </div>
 </template>
 
--- resources/js/Components/HeaderPage.vue
+++ resources/js/Components/HeaderPage.vue
@@ -1,52 +1,117 @@
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { ref } from "vue";
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  ]
+const Hambarg = ref(false);
+
 </script>
 
 <template>
-        <Disclosure as="nav" class="bg-[#f97316]">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div class="relative flex h-16 items-center justify-between">
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-              <!-- Profile dropdown -->
-              <Menu as="div" class="relative ml-3">
-                <div>
-                  <MenuButton class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span class="absolute -inset-1.5" />
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                  </MenuButton>
-                </div>
-                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                  <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-slot="{ active }">
-                      <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
-                    </MenuItem>
-                    <MenuItem v-slot="{ active }">
-                      <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
-                    </MenuItem>
-                    <MenuItem v-slot="{ active }">
-                      <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
-                    </MenuItem>
-                  </MenuItems>
-                </transition>
-              </Menu>
-            </div>
-          </div>
-        </div>
-
-        <DisclosurePanel class="sm:hidden">
-          <div class="space-y-1 px-2 pb-3 pt-2">
-            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
+  <div>
+    <div class="bg-orange-600 absolute z-10 w-full">
+      aaaaaaaaaaa
+    </div>
+    <div class="openbtn fixed top-0.5 right-0 md:right-2 z-50 cursor-pointer w-10 md:w-12 h-12" 
+    @click="Hambarg = !Hambarg" 
+    :class="{'active':Hambarg}">
+      <span class="inline-block absolute left-3 h-1 w-2/4 rounded-sm bg-orange-600 duration-500 top-2 md:top-4"></span>
+      <span class="inline-block absolute left-3 h-1 w-2/4 rounded-sm bg-orange-600 duration-500 top-4 md:top-6"></span>
+      <span class="inline-block absolute left-3 h-1 w-2/4 rounded-sm bg-orange-600 duration-500 top-6 md:top-8"></span>
+    </div>
+    <div id="g-nav" v-show="Hambarg" :class="{'panelactive':Hambarg}">
+      <ul class="opacity-0">
+        <li class="cursor-pointer text-white hover:text-slate-300 duration-500 pb-2 font-black text-lg"><a :href="a">プロフィール</a></li>
+        <li class="cursor-pointer text-white hover:text-slate-300 duration-500 pb-2 font-black text-lg"><a :href="a">お気に入り</a></li>
+        <li class="cursor-pointer text-white hover:text-slate-300 duration-500 pb-2 font-black text-lg"><a :href="a">投稿一覧</a></li>
+      </ul>
+    </div>
+    <div class="circle-bg bg-orange-400 fixed size-20 sm:size-24 rounded-full z-20 -right-12 -top-12 duration-700"
+    :class="{'circleactive':Hambarg}">
+    </div>
+    <div id="z-black" @click="Hambarg = !Hambarg" class="fixed w-full invisible duration-700" :class="{'bgaction':Hambarg}"></div>
+  </div>
 </template>
+
+<style scoped>
+#g-nav.panelactive{
+  position:fixed;
+  z-index: 30;
+  right: 3rem;
+  top: 3rem;
+}
+
+.circle-bg{
+	transform: scale(1.5);
+}
+
+.circle-bg.circleactive{
+	transform: scale(4.5);
+}
+
+#g-nav.panelactive #g-nav-list{
+  display: block;
+}
+
+/*ナビゲーション*/
+#g-nav.panelactive ul {
+    opacity:1;
+}
+
+#g-nav.panelactive ul li{
+animation-name:gnaviAnime;
+animation-duration:1s;
+animation-delay:.2s;
+animation-fill-mode:forwards;
+opacity:0;
+}
+@keyframes gnaviAnime{
+0% {
+opacity: 0;
+}
+100% {
+opacity: 1;
+}
+}
+
+/* メニュー背景 */
+#z-black.bgaction{
+  height: 100%;
+  visibility: visible;
+  background: rgba(0,0,0,0.4);
+  z-index: 10;
+}
+
+/*ボタンCSS*/
+.openbtn.active span:nth-of-type(1) {
+    top: 10px;
+    left: 18px;
+    transform: translateY(6px) rotate(-45deg);
+    width: 30%;
+}
+
+.openbtn.active span:nth-of-type(2) {
+	opacity: 0;
+}
+
+.openbtn.active span:nth-of-type(3){
+    top: 22px;
+    left: 18px;
+    transform: translateY(-6px) rotate(45deg);
+    width: 30%;
+}
+
+@media (min-width: 768px) {
+  .openbtn.active span:nth-of-type(1) {
+    top: 18px;
+    left: 18px;
+    transform: translateY(6px) rotate(-45deg);
+    width: 30%;
+  }
+
+  .openbtn.active span:nth-of-type(3){
+    top: 30px;
+    left: 18px;
+    transform: translateY(-6px) rotate(45deg);
+    width: 30%;
+  }
+}
+</style>
