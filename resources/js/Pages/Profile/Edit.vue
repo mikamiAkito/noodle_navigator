94c401e1c67a25ed2ac3ec4079ae5ce64dc578c5
unknown <akito.mikami17@gmail.com>
2024-03-21 00:37:49 +0900

    プロフィール編集

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -13,6 +13,10 @@ defineProps({
     profilePhotoUrl: String,
     coverPhotoUrl: String,
 });
+
+const profileUpdate = () => {
+//プロフィール画像編集処理
+};
 </script>
 
 <template>
@@ -25,9 +29,11 @@ defineProps({
                 <img :src="coverPhotoUrl" alt="User Cover"
                         class="w-full xl:h-[20rem] lg:h-[18rem] md:h-[16rem] sm:h-[14rem] xs:h-[11rem]" />
                 <!-- Profile Image -->
-                <div class="sm:w-[80%] xs:w-[90%] mx-auto flex">
+                <div class="sm:w-[80%] xs:w-[90%] mx-auto flex relative">
                     <img :src="profilePhotoUrl" alt="User Profile"
-                            class="rounded-md lg:w-[12rem] lg:h-[12rem] md:w-[10rem] md:h-[10rem] sm:w-[8rem] sm:h-[8rem] xs:w-[7rem] xs:h-[7rem] outline outline-2 outline-offset-2 outline-blue-500 relative lg:bottom-[5rem] sm:bottom-[4rem] xs:bottom-[3rem]" />
+                    class="rounded-md lg:w-[12rem] lg:h-[12rem] md:w-[10rem] md:h-[10rem] sm:w-[8rem] sm:h-[8rem] xs:w-[7rem] xs:h-[7rem] outline outline-2 outline-offset-2 outline-blue-500 relative lg:bottom-[5rem] sm:bottom-[4rem] xs:bottom-[3rem]" />
+                    <input type="file" @change="profileUpdate"
+                    class="absolute top-6 w-28 sm:w-32 md:top-14 md:w-40 lg:top-[4.5rem] lg:w-48 cursor-pointer">
                     <!-- FullName -->
                     <h1 class="w-full text-left my-4 sm:mx-4 xs:pl-4 text-gray-800 dark:text-white lg:text-4xl md:text-3xl sm:text-3xl xs:text-xl font-serif">
                         {{ $page.props.auth.user.name }}
